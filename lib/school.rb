88c3463e7afc18eae152c4a10bf9c5adf630b046
Your Name <you@example.com>
2017-02-08 17:07:48 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+class School
+  attr_accessor :school ,:roster
+
+  def initialize (school)
+    @school=school
+    @roster={}
+
+  end
+
+  def add_student(name,grade)
+    #@roster={grade=> [name]}
+    if @roster[grade]==nil
+    @roster[grade]=[]
+  end
+    @roster[grade]<<name
+  end
+
+  def grade (grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash={}
+    @roster.collect{|keys, values|
+      new_hash[keys]=values.sort
+    }
+    new_hash
+  end
+
+
+
+end
